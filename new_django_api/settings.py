70fd1d93c5b7c709da31231cf881fcef714af852
sntripathi180 <139564261+sntripathi180@users.noreply.github.com>
2025-08-24 16:52:54 +0530

    minor bug fix

--- new_django_api/settings.py
+++ new_django_api/settings.py
@@ -150,7 +150,7 @@ CORS_ALLOWED_ORIGINS = [
 "http://localhost:5174",
 "http://localhost:5175",
 "http://localhost:3000",
-
+"https://devfolio-xvbo.onrender.com",
 "https://devscribe.onrender.com",
-"https://devscribe-bxtb.onrender.com/"
+"https://devscribe-bxtb.onrender.com"
 ]
